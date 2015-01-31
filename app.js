6a9d94b65ec1804ae6c33ab77cbfb4ef3dd3cb50
Ryo Suzuki <ryooopan@googlemail.com>
2015-01-31 12:46:32 +0900

    add term.js

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 var express = require('express'),
+    http = require('http'),
     app = express();
+
+var server = http.createServer(app),
+    io = require('socket.io').listen(server),
+    term = require('term.js');
+    //terminal = require('../');
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+app.use(term.middleware());
+
+server.listen(app.get('port'), function(){
+  console.log("server listening on port " + app.get('port'));
+});
 
 app.get('/', function(req, res) {
   res.send('hello world');
@@ -7,7 +21,7 @@ app.get('/', function(req, res) {
 app.get('/about', function(req, res) {
   res.send('about');
 });
-
-app.listen(3000);
-console.log('server start');
-
+app.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  res.send('hello ' + name);
+});
